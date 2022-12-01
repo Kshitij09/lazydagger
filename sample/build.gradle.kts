19b8e35cc562063bce9b075a23c6cf20a69bbac6
Kshitij Patil <kshitijpatil98@gmail.com>
2022-12-01 16:45:55 +0530

    Add example sourceSets to make kapt work with ksp
    Signed-off-by: Kshitij Patil <kshitijpatil98@gmail.com>

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -4,6 +4,10 @@ plugins {
     alias(libs.plugins.ksp)
 }
 
+sourceSets.main {
+    java.srcDirs("build/generated/ksp/main/kotlin")
+}
+
 dependencies {
     implementation(project(":runtime"))
     implementation(libs.dagger)
